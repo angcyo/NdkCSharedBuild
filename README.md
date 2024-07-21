bbf8bd5eb72808d95dc58711c432b7d17885b16b
angcyo <angcyo@126.com>
2024-07-21 11:28:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2024-07-21
+
+`libc++_shared.so` build.
+
+https://developer.android.com/ndk/guides/cpp-support?hl=zh-cn&authuser=1
---
+++ app/src/main/java/com/angcyo/ndkcsharedbuild/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.angcyo.ndkcsharedbuild
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.angcyo.ndkcsharedbuild.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'ndkcsharedbuild' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'ndkcsharedbuild' library on application startup.
+        init {
+            System.loadLibrary("ndkcsharedbuild")
+        }
+    }
+}
